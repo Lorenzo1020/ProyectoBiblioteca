7bcb97e36f8175b6e96420d96cae0b9dc4106adc
Lorenzo1020 <ltecuatorres@gmail.com>
2025-06-16 10:15:33 -0600

    Se agregan nuevos cambios

--- src/Main/Principal.java
+++ src/Main/Principal.java
@@ -19,6 +19,10 @@ public class Principal {
 		int numPag, menuPrin, indice;
 		float precio;
 
+		// Realizando nueva funcionalidad
+		float numero1 = 3.1416f;
+		System.out.println("El resultado de pi es " + numero1);
+
 		Libros libro;
 
 		LogicaMetodos imp = new LogicaMetodos();
